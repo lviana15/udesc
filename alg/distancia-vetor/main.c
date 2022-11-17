9a5908698a6b00dc63b1df45839ae1b626b26cf5
Lucas Viana <lucasviana0604@gmail.com>
2022-11-16 23:31:52 -0300

    runcodes incomplete exercise

---
+++ alg/distancia-vetor/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+  int N, idx_N, distancia_N;
+  int V[10] = {9, 10, 200, 5, 6, 4, 70, 80, 91, 100};
+
+  scanf("%i", &N);
+
+  for (int i = 0; i < 10; i++){
+    if (V[i] == N) {
+      idx_N = i;
+      printf("%i", N);
+      printf("\n");
+      printf("Posicao %i", idx_N);
+      printf("\n");
+      break;
+    } else {
+      distancia_N = N - V[i];
+    } 
+  }
+} 
