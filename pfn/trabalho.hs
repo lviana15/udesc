3a027afb57208284e064532e5949e647f2e2cd66
Lucas Viana <lucasviana0604@gmail.com>
2022-11-24 19:48:39 -0300

    numLines function

--- pfn/trabalho.hs
+++ pfn/trabalho.hs
@@ -5,8 +5,10 @@ type Word' = String
 lines' :: Doc -> [Line]
 lines' x = lines x
 
---numLines :: [Line] -> [(Int, Line)]
---numLines 
+numLines :: [Line] -> [(Int, Line)]
+numLines (x:xs) = numLines' (x:xs) 1 
+numLines' [] y = []
+numLines' (x:xs) y = (y, x) : numLines' xs (y + 1) 
 --[(1, "Departamento de Ciencia da Computacao"), (2,"Curso de Ciencia da Computacao"), (3, "Programacao Funcional")]
 
 --allNumWords :: [(Int, Line)] -> [(Int,Word')]
