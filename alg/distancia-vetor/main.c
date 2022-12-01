45017b1fcdcce58f773c85bc6d504f869b3fb823
Lucas Viana <lucasviana0604@gmail.com>
2022-12-01 14:54:34 -0300

    Runcodes: Distance in vector

--- alg/distancia-vetor/main.c
+++ alg/distancia-vetor/main.c
@@ -2,24 +2,41 @@
 #include <stdlib.h>
 
 int main () {
-  int N, idx_N, distancia_N;
+  int N, distancia_N, menor_distancia;
   int V[10] = {9, 10, 200, 5, 6, 4, 70, 80, 91, 100};
 
   scanf("%i", &N);
+  menor_distancia = N - V[0];
 
   for (int i = 0; i < 10; i++){
+    if (N < V[i])
+      distancia_N = V[i] - N;
+    else
+      distancia_N = N - V[i];
+
     if (V[i] == N) {
-      idx_N = i;
       printf("%i", N);
       printf("\n");
-      printf("Posicao %i", idx_N);
+      printf("Posicao %i", i);
       printf("\n");
       break;
     } else {
-      distancia_N = N - V[i];
-      if (distancia_i == distancia_N) {
-        printf("Mais proximo %i posicao %i")
-      }
+      if (distancia_N < menor_distancia)
+        menor_distancia = distancia_N;
     } 
   }
-} 
+
+  for (int k = 0; k < 10; k++) {
+    if (N > V[k]){
+      if (N - V[k] == menor_distancia){
+        printf("Mais proximo %i posicao %i", V[k], k);
+        printf("\n");
+      }
+    } else {
+      if (V[k] - N == menor_distancia){
+        printf("Mais proximo %i posicao %i", V[k], k);    
+        printf("\n");
+      }
+    }
+  } 
+}
