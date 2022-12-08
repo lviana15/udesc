a02d6555db39ab727da3d72ec7cda5c4b23f7f2b
Lucas Viana <lucasviana0604@gmail.com>
2022-12-08 17:21:09 -0300

    Fix exercise

--- alg/prova2/questao2/main.c
+++ alg/prova2/questao2/main.c
@@ -18,24 +18,4 @@ int main() {
         printf("(%i) %i ===> primo\n", x, y);
         x++;
     }
-}include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    int x = 0, y;
-    int stop = 0;
-
-    while (stop == 0){
-        y = 3 * (x * x) + 39 * x + 37;
-
-        for (int i = 2; i < y/2; i++) {
-            if (y % i == 0) {
-                stop = 1;
-                break;
-            }
-        }
-        
-        printf("(%i) %i ===> primo\n", x, y);
-        x++;
-    }
 }
