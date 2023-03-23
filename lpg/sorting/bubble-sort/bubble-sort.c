19e3641555c224228539913c99863b8fbad3cdee
Lucas Viana <lucasviana0604@gmail.com>
2023-03-23 18:45:27 -0300

    simple analysis bubble-sort

--- lpg/sorting/bubble-sort/bubble-sort.c
+++ lpg/sorting/bubble-sort/bubble-sort.c
@@ -14,7 +14,7 @@ int main() {
   srand(time(NULL));
 
   int vec[100];
-  int i, n; 
+  int i, n, count, cound, ordered;
 
   printf("Digite o valor de n:");
   scanf("%d", &n);
@@ -24,14 +24,24 @@ int main() {
   }
 
   for(i = 0;i < n;i++) {
+    ordered = 0;
     for(int j = 0;j < n - i - 1;j++) {
-      if (vec[j] > vec[j+1]) swap(&vec[j], &vec[j+1]);
+      if (vec[j] > vec[j+1]) {
+        swap(&vec[j], &vec[j+1]);
+        ordered = 1;
+        count++;
+      }
+      cound++;
     }
+    if (ordered == 0) break;
   }
 
   for(i = 0;i < n;i++) {
     printf("%d\t", vec[i]);
   }
+
+  printf("\nContagem de verificações: %d", count);
+  printf("\nContagem: %d", cound);
   
   return 0;
 }
