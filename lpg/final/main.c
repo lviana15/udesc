e625d69eb1e2da91bab2bfc4b984e28dd4a9214a
Lucas Viana <lucasviana0604@gmail.com>
2023-06-17 16:33:51 -0300

    Structs declaration

--- lpg/final/main.c
+++ lpg/final/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct data {
+  int dia;
+  int mes;
+  int ano;
+};
+
+struct horario {
+  int hora;
+  int minuto;
+};
+
+struct evento {
+  struct data data_jogo;
+  struct horario horario_inicio;
+  struct horario horario_fim;
+  char descicao[10];
+  char local[20];
+};
+
 int main() {
 
   return 0;
