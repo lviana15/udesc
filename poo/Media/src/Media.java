7a951e1c9f43d7c201ffabbfa993992f6bdda8b5
Lucas Viana <lucasviana0604@gmail.com>
2023-08-02 23:53:33 -0300

    correct media

---
+++ poo/Media/src/Media.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Media {
+	public static void main(String[] args) {
+        int[] arr = new int[5];
+        int sum = 0;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite 5 valores:");
+        for(int i = 0;i < 5;i++){
+            arr[i] = Integer.parseInt(in.nextLine());
+        }
+
+        for(int i = 0;i < 5;i++){
+            sum += arr[i];
+        }
+
+        System.out.println("Media dos valores: " +sum/5);
+	}
+}
