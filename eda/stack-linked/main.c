e034d1730b6b42c2e640439bce7baa115eaf8c98
Lucas Viana <lucasviana0604@gmail.com>
2023-09-20 12:59:04 -0300

    DSA: fixed my stack implementation

--- eda/stack-linked/main.c
+++ eda/stack-linked/main.c
@@ -5,17 +5,21 @@
 int main (void) {
     Stack *stack = createStack();
 
-    isEmpty(stack);
-
     push(stack, 5);
     push(stack, 10);
     peek(stack);
-
-    push(stack, 7);
-    push(stack, 21);
+    printStack(stack);
 
     printf("Stack size: %d\n", size(stack));
 
+    push(stack, 7);
+    push(stack, 21);
+    printf("Stack size: %d\n", size(stack));
+
+    pop(stack);
+    pop(stack);
+
+    printf("Stack size: %d\n", size(stack));
     printStack(stack);
 
     freeStack(stack);
--- eda/stack-linked/stack.c
+++ eda/stack-linked/stack.c
@@ -12,6 +12,7 @@ Node *createNode(int value) {
 
 Stack *createStack() {
   Stack *stack = malloc(sizeof(Stack));
+  stack->top = NULL;
   return stack;
 }
 
@@ -23,45 +24,62 @@ void push(Stack *stack, int value) {
   stack->top = newNode;
 }
 
+void pop(Stack *stack) {
+  if (!stack->top) {
+    printf("Empty stack");
+    return;
+  }
+
+  Node *aux = stack->top;
+  stack->top = aux->next;
+  free(aux);
+}
+
 void peek(Stack *stack) {
-    if (isEmpty(stack)) printf("Empty stack");
-    else printf("[%d]", stack->top->data);
-    printf("\n");
+  if (isEmpty(stack))
+    printf("Empty stack");
+  else
+    printf("[%d]", stack->top->data);
+  printf("\n");
 }
 
 int size(Stack *stack) {
-    int size = 0;
-    Node *aux = stack->top;
+  int size = 0;
+  Node *aux = stack->top;
 
-    while(aux != NULL){
-        size++;
-        aux = aux->next;
-    }
+  while (aux != NULL) {
+    size++;
+    aux = aux->next;
+  }
 
-    return size;
+  return size;
 }
 
 int isEmpty(Stack *stack) {
-    if (stack->top == NULL) return 1;
-    else return 0;
+  if (stack->top == NULL)
+    return 1;
+  else
+    return 0;
 }
 
 void printStack(Stack *stack) {
-  while (stack->top != NULL) {
-    printf("[%d] ", stack->top->data);
-    stack->top = stack->top->next;
+  Node *aux = stack->top;
+  while (aux != NULL) {
+    printf("[%d] ", aux->data);
+    aux = aux->next;
   }
 
   printf("\n");
 }
 
 void freeStack(Stack *stack) {
-    while (stack->top != NULL) {
-        Node *aux = stack->top;
-        free(aux);
-        stack->top = aux->next;
-    }
+  Node *aux = stack->top->next;
+  while (aux != NULL) {
+    free(stack->top);
+    stack->top = aux;
+    aux = aux->next;
+  }
 
-    free(stack);
+  free(stack->top);
+  free(stack);
 }
-
